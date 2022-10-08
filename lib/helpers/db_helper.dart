0f429e5a0282ed65438b7c6f4321a16ad2483110
AERM2018 <retana.martinez.angel.eduardo@gmail.com>
2022-10-08 12:05:20 -0500

    Done: RMA-3 - Update db migrations on create

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:rigel_app/models/product_category_model.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
@@ -23,13 +24,13 @@ class DbHelper{
     await db.execute(
       '''
         CREATE TABLE productCategories(
-          id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+          id INTEGER PRIMARY KEY,
           name VARCHAR(70) NOT NULL,
-          iconName VARCHAR(50);
+          iconName VARCHAR(50)
         );
 
         CREATE TABLE products(
-          id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+          id INTEGER PRIMARY KEY,
           categoryId INTEGER NOT NULL,
           price FLOAT NOT NULL,
           ranking INTEGER NOT NULL DEFAULT 0,
@@ -38,12 +39,16 @@ class DbHelper{
           calories FLOAT NOT NULL,
           additives FLOAT NOT NULL,
           vitamins FLOAT NOT NULL,
-          CONSTRAINT fk_category FOREIGN KEY(categoryId) REFERENCES productCategories(id); 
+          CONSTRAINT fk_category FOREIGN KEY(categoryId) REFERENCES productCategories(id)
         );
-
-        INSERT INTO productCategories VALUES(DEFAULT,'Nuts',NULL);
-        INSERT INTO productCategories VALUES(DEFAULT,'Dried friut',NULL);
       '''
     );
+
+    await _createDefaultCategories(db);
+  }
+
+  Future<void> _createDefaultCategories(Database database) async {
+    await database.insert('productCategories', ProductCategory(id:1,name: "Dried fruits").toMap());
+    await database.insert('productCategories', ProductCategory(id:2,name: "Nuts").toMap());
   }
 }
