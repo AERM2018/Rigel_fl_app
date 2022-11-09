03d854d0d4b68b80986faa86da922a5877d2f4ca
AERM2018 <retana.martinez.angel.eduardo@gmail.com>
2022-11-09 00:21:27 -0600

    Done: RMA-26 - Add config files with firebasefire - Add firebase_core dependecie

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rigel_app/providers/providers.dart';
 import 'package:rigel_app/screens/screens.dart';
 import 'package:rigel_app/themes/app_theme.dart';
 
-void main(){
+import 'firebase_options.dart';
+
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
